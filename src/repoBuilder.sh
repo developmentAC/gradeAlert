d9d143eb276888e1532ee4ea5c457dfa4933c7e6
Oliver Bonham-Carter <31265642+obonhamcarter@users.noreply.github.com>
2022-11-02 12:45:56 -0400

    Update repoBuilder.sh
    fix: file instructions now mirror readme.md examples.

--- src/repoBuilder.sh
+++ src/repoBuilder.sh
@@ -1,6 +1,6 @@
 # add the git clone statements here to clone the gradebook repositories. Once the repositories have been cloned, they are copied into a student_repos directory. This file can be edited as necessary.
 
 
-mkdir student_repos
-mv gradebook* student_repos/
+mkdir studentGradeBook_Repos
+mv gradebook* studentGradeBook_Repos/
 
